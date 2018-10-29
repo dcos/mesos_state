a8e187a9cf9cb86b3fb6e1adb24e42274cfbdc9e
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2018-10-29 12:24:15 -0700

    Add task health check information

--- src/mesos_state_client.erl
+++ src/mesos_state_client.erl
@@ -240,7 +240,8 @@ task(Task, Framework, Slaves) ->
     container = container(maps:get(container, Task, undefined)),
     discovery = discovery(maps:get(discovery, Task, undefined)),
     slave = Slave,
-    framework = framework(Framework)
+    framework = framework(Framework),
+    health_check = health_check(Task)
   }.
 
 task_labels(Labels) ->
@@ -453,6 +454,11 @@ protocol(BinString) when is_binary(BinString) ->
         "tcp" -> tcp;
         "udp" -> udp
     end.
+
+health_check(#{health_check := HealthCheck}) when is_map(HealthCheck) ->
+    HealthCheck;
+health_check(_Task) ->
+    undefined.
 
 -ifdef(TEST).
 
